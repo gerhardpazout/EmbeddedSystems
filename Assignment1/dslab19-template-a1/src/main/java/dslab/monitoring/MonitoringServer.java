5ebc338e255f3f1b72fe1abc47ab878ea47cc3ab
Gerhard Pazout <gerhard.pazout@gmx.at>
2019-10-29 20:07:02 +0100

    [FEATURE] Add bootup message to monitoring server

--- Assignment1/dslab19-template-a1/src/main/java/dslab/monitoring/MonitoringServer.java
+++ Assignment1/dslab19-template-a1/src/main/java/dslab/monitoring/MonitoringServer.java
@@ -1,13 +1,19 @@
 package dslab.monitoring;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import dslab.ComponentFactory;
 import dslab.util.Config;
 
 public class MonitoringServer implements IMonitoringServer {
 
+    private String componentId;
+    private Config config;
+    private ServerSocket serverSocket;
     /**
      * Creates a new server instance.
      *
@@ -17,12 +23,37 @@ public class MonitoringServer implements IMonitoringServer {
      * @param out the output stream to write console output to
      */
     public MonitoringServer(String componentId, Config config, InputStream in, PrintStream out) {
-        // TODO
+        this.componentId = componentId;
+        this.config = config;
+        try {
+            serverSocket = new ServerSocket(config.getInt("udp.port"));
+            printBootUpMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printBootUpMessage(){
+        System.out.println("Monitoring Server '" + componentId + "' online. \n" +
+                "\tUDP on port " + config.getInt("udp.port") + "\n"
+        );
+        System.out.println(
+                "Use command 'nc -u localhost " +
+                        config.getInt("udp.port") +
+                        "' in terminal app to connect"
+        );
     }
 
     @Override
     public void run() {
-        // TODO
+        try {
+            Socket socket = serverSocket.accept();
+
+            System.out.println("Client connected");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
