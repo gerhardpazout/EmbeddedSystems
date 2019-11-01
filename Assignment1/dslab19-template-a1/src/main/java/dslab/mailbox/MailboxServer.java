1063d4648322a62d82f1b34e1f76117bb87397ce
Gerhard Pazout <gerhard.pazout@gmx.at>
2019-11-01 17:42:36 +0100

    [FEATURE] Implement two socket functionality (clients & mailbox)

--- Assignment1/dslab19-template-a1/src/main/java/dslab/mailbox/MailboxServer.java
+++ Assignment1/dslab19-template-a1/src/main/java/dslab/mailbox/MailboxServer.java
@@ -1,11 +1,12 @@
 package dslab.mailbox;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
+import at.ac.tuwien.dsg.orvell.Shell;
 import dslab.ComponentFactory;
+import dslab.util.CommandLine;
 import dslab.util.Config;
 
 public class MailboxServer implements IMailboxServer, Runnable {
@@ -14,6 +15,10 @@ public class MailboxServer implements IMailboxServer, Runnable {
     private Config config;
     private ServerSocket serverSocketDMTP;
     private ServerSocket serverSocketDMAP;
+    private CommandLine shellClient;
+    private InputStream in;
+    private OutputStream out;
+    private Socket transferSocket;
 
     /**
      * Creates a new server instance.
@@ -27,9 +32,12 @@ public class MailboxServer implements IMailboxServer, Runnable {
         // TODO
         this.componentId = componentId;
         this.config = config;
+        this.in = in;
+        this.out = out;
+
         try {
             this.serverSocketDMTP = new ServerSocket(config.getInt("dmtp.tcp.port"));
-            this.serverSocketDMAP = new ServerSocket(config.getInt("dmap.tcp.port"));
+            //this.serverSocketDMAP = new ServerSocket(config.getInt("dmap.tcp.port"));
             printBootUpMessage();
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,19 +46,47 @@ public class MailboxServer implements IMailboxServer, Runnable {
 
     private void printBootUpMessage(){
         System.out.println("Mailbox Server '" + componentId + "' online. \n" +
-                "\tDMTP on port " + config.getInt("dmtp.tcp.port") + "\n" +
-                "\tDMAP on port " + config.getInt("dmap.tcp.port") + "\n"
+                "\tDMTP via TCP on port " + config.getInt("dmtp.tcp.port") + "\n" +
+                "\tDMAP via TCP on port " + config.getInt("dmap.tcp.port") + "\n"
         );
         System.out.println(
                 "Use command 'nc " +
                         config.getString("registry.host") + " <port number>" +
-                        "' in terminal app to connect"
+                        "' in terminal app to connect \n"
         );
+        System.out.println(componentId + " logs:");
+    }
+
+    public ServerSocket getServerSocketDMTP() {
+        return serverSocketDMTP;
+    }
+
+    public ServerSocket getServerSocketDMAP(){
+        return serverSocketDMAP;
     }
 
     @Override
     public void run() {
         // TODO
+        //connection to transfer server (functioning)
+        try {
+            transferSocket = serverSocketDMTP.accept();
+
+            InputStreamReader isr = new InputStreamReader(transferSocket.getInputStream());
+            BufferedReader bfr = new BufferedReader(isr);
+
+            String message = bfr.readLine();
+            System.out.println("transfer server: " + message);
+
+            //Send message to incoming device
+            PrintWriter pr = new PrintWriter(transferSocket.getOutputStream());
+
+            pr.println("Mailbox Server: Welcome! Type 'disconnect' to exit.");
+            pr.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- Assignment1/dslab19-template-a1/src/main/java/dslab/transfer/TransferServer.java
+++ Assignment1/dslab19-template-a1/src/main/java/dslab/transfer/TransferServer.java
@@ -3,6 +3,7 @@ package dslab.transfer;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import dslab.ComponentFactory;
 import dslab.util.Config;
@@ -11,7 +12,6 @@ public class TransferServer implements ITransferServer, Runnable {
 
     private String componentId;
     private Config config;
-    private ServerSocket serverSocket;
     private InputStream in;
     private PrintStream out;
 
@@ -27,41 +27,19 @@ public class TransferServer implements ITransferServer, Runnable {
         // TODO
         this.componentId = componentId;
         this.config = config;
-        this.in = this.in;
+        this.in = in;
         this.out = out;
-
-        //cli = new CommandLine("cli-transfer-1", config, in, out);
-
-        try {
-            serverSocket = new ServerSocket(config.getInt("tcp.port"));
-            printBootUpMessage();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void run() {
         // TODO
+        Thread clientSocketHandler = new ClientSocketHandler(config.getInt("tcp.port"));
+        Thread mailboxSocketHandler = new MailboxSocketHandler("localhost", 11482);
 
-        //run infinite loop to get all client requests (e.g. multiple client connections)
-        while(true){
-            try {
-                Socket socketClient = serverSocket.accept(); //accept incoming request / get the socket from incoming device
-
-                // create a new thread object to allow multiple clients
-                Thread client = new ClientHandler(socketClient, socketClient.getInputStream(), socketClient.getOutputStream());
-                //clientThreads.add(client);
-                // Invoking the start() method
-                client.start();
-
-                // additional code...
-
-            } catch (IOException e) {
-                e.printStackTrace();
-
-            }
-        }
+        clientSocketHandler.start();
+        mailboxSocketHandler.start();
+        printBootUpMessage();
     }
 
     private void printBootUpMessage(){
@@ -70,33 +48,102 @@ public class TransferServer implements ITransferServer, Runnable {
                 "Use command 'nc " +
                         config.getString("registry.host") + " " +
                         config.getInt("tcp.port") +
-                        "' in terminal app to connect"
+                        "' in terminal app to connect \n"
         );
+        System.out.println(componentId + " logs:");
     }
 
     @Override
     public void shutdown() {
         // TODO
+        /*
         try {
             System.out.println("shutting down connection");
             serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
     }
 
     public static void main(String[] args) throws Exception {
         ITransferServer server = ComponentFactory.createTransferServer(args[0], System.in, System.out);
-        //server.run();
-        new Thread(server).start();
+        server.run();
     }
-
 }
 
-// ClientHandler class
+//Mailbox Server
+class MailboxSocketHandler extends Thread {
+    private InputStream in;
+    private OutputStream out;
+    private Socket socket;
+    private String host;
+    private int port;
+
+    // Constructor
+    public MailboxSocketHandler(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public void run()
+    {
+        try {
+            Socket socket = new Socket(host, port);
+
+            //Receiver message from incoming device
+            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+            BufferedReader bfr = new BufferedReader(isr);
+
+            //Send message to incoming device
+            PrintWriter pr = new PrintWriter(socket.getOutputStream());
+
+            pr.println("Whats up mailbox server?");
+            pr.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//Clients
+class ClientSocketHandler extends Thread {
+
+    ServerSocket serverSocket;
+
+    public ClientSocketHandler(int port){
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            try {
+                //accept incoming request / get the socket from incoming device
+                Socket socketClient = serverSocket.accept();
+                System.out.println("client connected");
+
+                // create a new thread object to allow multiple clients
+                Thread client = new ClientHandler(socketClient, socketClient.getInputStream(), socketClient.getOutputStream());
+
+                // Invoking the start() method
+                client.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 class ClientHandler extends Thread{
     private InputStream in;
     private OutputStream out;
+    private ServerSocket serverSocket;
     private Socket socket;
 
     // Constructor
@@ -111,8 +158,6 @@ class ClientHandler extends Thread{
     public void run()
     {
         try {
-            System.out.println("client connected");
-
             //Receiver message from incoming device
             InputStreamReader isr = new InputStreamReader(socket.getInputStream());
             BufferedReader bfr = new BufferedReader(isr);
@@ -151,5 +196,6 @@ class ClientHandler extends Thread{
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
